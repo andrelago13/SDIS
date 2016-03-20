40d454cf574a904503b6bd68d7711da36c7ed55a
up201305980 <up201305980@fe.up.pt>
2016-03-20 11:54:19 +0000

    TestApp

---
+++ src/backupservice/cli/TestApp.java
@@ -0,0 +1,92 @@
+package backupservice.cli;
+
+import java.util.Scanner;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		boolean run = true;
+		while(run)
+		{
+			String in = sc.nextLine();
+			String[] commandInfo = null;
+			String command = checkCommand(in, commandInfo);
+
+			switch(command)
+			{
+			case "BACKUP":
+				// Execute backup protocol with commandInfo
+				break;
+			case "RESTORE":
+				// Execute restore protocol with commandInfo
+				break;
+			case "RECLAIM":
+				// Execute reclaim protocol with commandInfo
+				break;
+			case "DELETE":
+				// Execute delete protocol with commandInfo
+				break;
+			case "ERROR":
+				System.out.println("Invalid operation!!! Type -> java TestApp HELP\n for more help");
+				break;
+			case "EXIT":
+				run = false;
+				break;
+			case "HELP":
+				System.out.println("Protocols availables:\n\n" + "Backup file:\n Press:BACKUP file replicationDegree\n\n"
+						+ "Restore file:\n Press:RESTORE file\n\n" 
+						+ "Reclaim file:\n Press:RECLAIM space\n\n"
+						+ "Delete file:\n Press:DELETE file\n\n"
+						+ "exit:\n Press:EXIT\n\n");
+				break;
+			}	
+		}
+		sc.close();
+	}
+
+	public static String checkCommand (String input, String[] inputInfo)
+	{
+		String[] inputParts = input.split(" ");
+		String response = "";
+
+
+		// TODO Change min and max replication degree of the file 
+		if(inputParts[0].equals("BACKUP") && inputParts.length == 3 && utils.Files.fileValid(inputParts[1]) && Integer.parseInt(inputParts[2]) > 1 && Integer.parseInt(inputParts[2]) < 6)
+		{
+			response = "BACKUP";
+			// file name to backup
+			inputInfo[0] = inputParts[1];
+			// replication degree
+			inputInfo[1] = inputParts[2];
+		}
+		else if(inputParts[0].equals("RESTORE") && inputParts.length == 2)
+		{
+			response = "RESTORE";
+			// file name to be restored
+			inputInfo[0] = inputParts[1];
+		}
+		else if(inputParts[0].equals("RECLAIM") && inputParts.length == 2)
+		{
+			response = "RECLAIM";
+			// space reclaimed
+			inputInfo[0] = inputParts[1];
+		}
+		else if(inputParts[0].equals("DELETE") && inputParts.length == 2)
+		{
+			response = "DELETE";
+			// file to be deleted
+			inputInfo[0] = inputParts[1];
+		}
+		else if(inputParts[0].equals("HELP"))
+			response = "HELP";
+		else if(inputParts[0].equals("EXIT"))
+			response = "EXIT";
+		else
+			response = "ERROR";
+
+		return response;
+	}
+
+}
