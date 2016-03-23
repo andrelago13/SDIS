948fe805f16da8245903b91055ca21cbbef21295
up201305980 <up201305980@fe.up.pt>
2016-03-23 17:54:31 +0000

    Tcp included on TestApp

--- src/backupservice/cli/TestApp.java
+++ src/backupservice/cli/TestApp.java
@@ -1,5 +1,11 @@
 package backupservice.cli;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class TestApp {
@@ -9,44 +15,68 @@ public class TestApp {
 	public static String LocalIp;
 	public static int LocalPort;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException, IOException {
 
-		String[] commandInfo = null;
+		String[] commandInfo = new String[args.length - 2];
 		String command = checkCommand(args, commandInfo);
+
+		Socket socket = new Socket(LocalIp, LocalPort);
+		PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+		BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+		String request = "";
+
+		switch(command)
+		{
+		case "BACKUP":
+			// Execute backup protocol with commandInfo
+			request += "BACKUP " + commandInfo[0] + " " + commandInfo[1];
+			break;
+		case "RESTORE":
+			// Execute restore protocol with commandInfo
+			request += "RESTORE " + commandInfo[0];
+			break;
+		case "RECLAIM":
+			// Execute reclaim protocol with commandInfo
+			request += "RECLAIM " + commandInfo[0];
+			break;
+		case "DELETE":
+			// Execute delete protocol with commandInfo
+			request += "DELETE " + commandInfo[0];
+			break;
+		case "ERROR":
+			System.out.println("Invalid operation!!! Type -> java TestApp HELP\n for more help");
+			break;
+		case "EXIT":
+			break;
+		case "HELP":
+			System.out.println("Protocols availables:\n\n" + "Backup file:\n Press:BACKUP peer_ap file replicationDegree\n\n"
+					+ "Restore file:\n Press:RESTORE peer_ap file\n\n" 
+					+ "Reclaim file:\n Press:RECLAIM peer_ap space\n\n"
+					+ "Delete file:\n Press:DELETE peer_ap file\n\n"
+					+ "exit:\n Press:EXIT\n\n");
+			break;
+		}
 		
-			switch(command)
-			{
-			case "BACKUP":
-				// Execute backup protocol with commandInfo
-				break;
-			case "RESTORE":
-				// Execute restore protocol with commandInfo
-				break;
-			case "RECLAIM":
-				// Execute reclaim protocol with commandInfo
-				break;
-			case "DELETE":
-				// Execute delete protocol with commandInfo
-				break;
-			case "ERROR":
-				System.out.println("Invalid operation!!! Type -> java TestApp HELP\n for more help");
-				break;
-			case "EXIT":
-				break;
-			case "HELP":
-				System.out.println("Protocols availables:\n\n" + "Backup file:\n Press:BACKUP peer_ap file replicationDegree\n\n"
-						+ "Restore file:\n Press:RESTORE peer_ap file\n\n" 
-						+ "Reclaim file:\n Press:RECLAIM peer_ap space\n\n"
-						+ "Delete file:\n Press:DELETE peer_ap file\n\n"
-						+ "exit:\n Press:EXIT\n\n");
-				break;
-			}	
+		output.println(request);
+		System.out.println("Request sent -> " + request);
+		
+		String resp = input.readLine();
+		System.out.println("Response received -> " + resp);
+		
+		if(resp.equals("0"))
+			System.out.println("Command successful!");
+		else
+			// resp has the error code
+			System.err.println("Command unsuccessful (" + resp + ")");
+
+		output.close();
+		input.close();
 	}
 
 	public static String checkCommand (String[] args, String[] inputInfo)
 	{
 		String[] inputParts = new String[args.length];
-		inputInfo = new String[args.length - 2];
 
 		for(int i = 0; i < args.length; i++)
 			inputParts[i] = args[i];
