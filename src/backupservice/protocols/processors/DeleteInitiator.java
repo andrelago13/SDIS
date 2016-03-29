870ac05301b371859d80bfc258bbcb95d53f6c69
up201305980 <up201305980@fe.up.pt>
2016-03-29 19:27:03 +0100

    DeleteInitiator

--- src/backupservice/protocols/processors/DeleteInitiator.java
+++ src/backupservice/protocols/processors/DeleteInitiator.java
@@ -13,6 +13,7 @@ import backupservice.BackupService;
 import backupservice.protocols.ProtocolHeader;
 import backupservice.protocols.ProtocolInstance;
 import backupservice.protocols.Protocols;
+import backupservice.protocols.processors.RestoreInitiator.EndCondition;
 
 public class DeleteInitiator implements ProtocolProcessor {
 
@@ -27,7 +28,10 @@ public class DeleteInitiator implements ProtocolProcessor {
 
 	private Socket responseSocket = null;
 	private Boolean active = false;
+	private Boolean present = false;
 
+	private int current_attempt = 0;
+	
 	public DeleteInitiator(BackupService service, String filePath) {
 		this(service, filePath, null);
 	}
@@ -36,59 +40,47 @@ public class DeleteInitiator implements ProtocolProcessor {
 		this.service = service;
 		this.filePath = filePath;
 		this.responseSocket = response_socket;
-	}	
+	}
 
 	@Override
 	public void initiate() {
 		
-		// TODO ver se ficheiro foi backed up (ver metadata)
-		// TODO		se não estiver, concluir com mensagem apropriada
-		// TODO		se estiver, apagar o ficheiro, apagas a metadata e envias DELETE (3x, 2s)
-		// TODO		terminas
+		// TODO ver se ficheiro foi backed up (ver metadata) ok
+		// TODO		se não estiver, concluir com mensagem apropriada ok
+		// TODO		se estiver, apagar o ficheiro, apagas a metadata e envias DELETE (3x, 2s) ?
+		// TODO		terminas ok
 
-		/*MetadataManager mg = service.getMetadata();
-
-		// Testou-se com um ficheiro já criado
-		//	mg.ownFilesInfo().add(new FileBackupInfo("resources/test_read.txt", "12345"));
-
-
-		int i = 0;
-		for(; i < mg.ownFilesInfo().size(); i++)
-			if(mg.ownFilesInfo().get(i).getFilePath().equals(filePath))
-			{
-				if(utils.Files.fileValid(filePath))
-				{
-					utils.Files.removeFile(filePath);
-					break;
+		service.logAndShow("Starting DeleteInitiator...");
+		
+		MetadataManager mg = service.getMetadata();
+		
+		deleteFile(mg, filePath);
+		
+		try {
+			notifyDelete(mg);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			
+			if(responseSocket != null) {
+				try {
+					SocketWrapper.sendTCP(responseSocket, "" + EndCondition.FILE_WAS_NOT_BACKED_UP.ordinal());
+				} catch (IOException e) {
+					e.printStackTrace();
+					service.logAndShowError("Unable to notify initiator of DELETE protocol");
 				}
 			}
-		// falta apagar metadata
-
-		if(i == mg.ownFilesInfo().size())
-			service.logAndShow("File provided by filePath doesn't exist!");
-
-		if(responseSocket != null)
-			try {
-				SocketWrapper.sendTCP(responseSocket, condition_codes[EndConditionD.CANNOT_DELETE_FILE.ordinal()]);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-		ArrayList<FileBackupInfo> peerFiles = mg.peerFilesInfo();
-		//		peerFiles.add(new FileBackupInfo("resources/test_join", "12945"));
-
-		for(int j = 0; j < peerFiles.size(); j++)
-			if(peerFiles.get(j).getFilePath().equals(filePath))
-				removers.add(new ChunkRemove(service, peerFiles.get(j).getChunks().get(j), peerFiles.get(j).getHash()));
-
-		if(removers.size() != 0)
+		}
+		terminate();
+	}
+	
+	public void notifyDelete(MetadataManager mg) throws IOException
+	{
+		if(present)
 		{
-			active = true;
-
-			for(int l = 0; l < removers.size(); l++) {
-				removers.get(l).start();
-			}
-		}*/
+			ProtocolInstance instance = Protocols.deleteProtocolInstance(Protocols.PROTOCOL_VERSION_MAJOR, Protocols.PROTOCOL_VERSION_MINOR,
+					service.getIdentifier(), mg.ownFileBackupInfo_path(filePath).getHash());
+				service.getControlSocket().send(instance.toString());
+		}
 	}
 
 	@Override
@@ -103,5 +95,22 @@ public class DeleteInitiator implements ProtocolProcessor {
 	public void terminate() {
 		active = false;
 		service.removeProcessor(this);
+	}
+	
+	public void deleteFile(MetadataManager mg, String pathFile)
+	{
+		if(mg.ownFileBackupInfo_path(pathFile) != null)
+				if(utils.Files.fileValid(pathFile))
+				{
+					present = true;
+					// Remove file
+					utils.Files.removeFile(pathFile);
+					// Remove file from metadata
+					mg.ownFilesInfo().remove(mg.ownFileBackupInfo_path(pathFile));
+					service.logAndShow("File provided by filePath was successfully deleted!");
+				}
+				else
+					service.logAndShow("File provided by filePath doesn't exist!");
+					
 	}
 }
