426efd01c4232829004ad67f0d5dc8c65e2cb471
aslmbc13 <asl_mbc_13@hotmail.com>
2016-04-02 00:50:34 +0100

    Backup initiator fix

--- src/network/SocketWrapper.java
+++ src/network/SocketWrapper.java
@@ -7,15 +7,26 @@ import java.net.Socket;
 public class SocketWrapper {
 	
 	public static void sendTCP(Socket socket, String message) throws IOException {
+		sendTCP(socket, message, true);
+	}
+	
+	public static void sendTCP(Socket socket, String message, Boolean close) throws IOException {
 		DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
 		outToClient.writeBytes(message);
-		outToClient.close();
+		if(close)
+			outToClient.close();		
 	}
 	
 	public static void sendTCP(Socket socket, byte[] message) throws IOException {
+		sendTCP(socket, message, true);
+	}
+	
+	public static void sendTCP(Socket socket, byte[] message, Boolean close) throws IOException {
 		DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
 		outToClient.write(message);
-		outToClient.close();
+		if(close)
+			outToClient.close();
+		
 	}
 	
 }
