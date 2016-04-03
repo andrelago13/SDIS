464d3267491c6ca08fe985a1ac9b2a5fadbfbd26
Andre Sousa Lago <up201303313@B02AU0802.fe.up.pt>
2016-04-03 18:17:04 +0100

    Fix

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-mkdir bin
-javac -d bin -sourcepath src -encoding ISO-8859-1 src/backupservice/cli/BackupServiceCLI.java src/backupservice/cli/TestApp.java
+mkdir -p bin
+javac -d bin -sourcepath src -encoding ISO-8859-1 src/backupservice/cli/TestApp.java src/backupservice/cli/BackupServiceCLI.java
